4c1069d4489da835918a2f1fe80cb0ba81ae4daa
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-09 19:48:49 +0900

    [D1] Title: 스탬프 찍기, Time: 122 ms, Memory: 20,416 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = Integer.parseInt(sc.nextLine());
+	
+		for (int i = 0; i < num; i++ ) {
+			System.out.print('#');
+		}
+	}
+
+}
