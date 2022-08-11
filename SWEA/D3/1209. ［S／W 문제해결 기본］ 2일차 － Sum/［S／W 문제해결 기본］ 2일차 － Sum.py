653d38b0d9082039b87d97cadd7ba30677918701
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-11 17:06:31 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 234 ms, Memory: 62,784 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -3,38 +3,21 @@ for _ in range(10):
     arr = [list(map(int, input().split())) for _ in range(100)]
     result = 0
 
-    # 행
     for i in range(100):
-        sum_num = 0
+        # 합 리스트
+        sums = [0, 0, 0, 0] # 행 열 대각선왼위오밑 대각선오위왼밑
+
         for j in range(100):
-            sum_num += arr[i][j]
-        if sum_num > result:
-            result = sum_num
+            sums[0] += arr[i][j]
+            sums[1] += arr[j][i]
 
-    # 열
-    for j in range(100):
-        sum_num = 0
-        for i in range(100):
-            sum_num += arr[i][j]
-        if sum_num > result:
-            result = sum_num
+        sums[2] += arr[i][i] # 대각선: 왼위 -> 오밑
+        sums[3] += arr[i][100 - i - 1] # 대각선 오위 -> 왼밑
+        
+        # 합 중 최댓값 구하기
+        for sum in sums:
+            if sum > result:
+                result = sum
 
-    # 대각선 왼위 -> 오밑
-    j = -1
-    sum_num = 0
-    for i in range(100):
-        j += 1
-        sum_num += arr[i][j]
-    if sum_num > result:
-        result = sum_num
-
-    # 대각선 외위 -> 왼밑
-    j = 100
-    sum_num = 0
-    for i in range(100):
-        j -= 1
-        sum_num += arr[i][j]
-    if sum_num > result:
-        result = sum_num
 
     print(f'#{tc} {result}')
