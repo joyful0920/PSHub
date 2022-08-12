71ead177383151f396c27d1cce3f5f94a0c8f0d4
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-12 17:43:06 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 214 ms, Memory: 63,040 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,40 +1,17 @@
-dx = [0, 0, 1]
+dx = [0, 0, -1]
 dy = [-1, 1, 0]
 
-for _ in range(1, 11):
+for _ in range(10):
     tc = int(input())
     ladders = [list(map(int, input().split())) for _ in range(100)]
-    starts = [i for i in range(100) if ladders[0][i]]
+    end = [99] + [i for i in range(100) if ladders[99][i] == 2]
 
-    for sy in starts:
-        x, y = 0, sy
-        toggle = True
-        yd = 0
-        while x < 99:
-            if toggle:
-                for i in range(2):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if 0 <= nx < 100 and 0 <= ny < 100 and ladders[nx][ny]:
-                        x = nx
-                        y = ny
-                        yd = dy[i]
-                        toggle = not toggle
-                        break
-                if toggle:
-                    x += 1
-            else:
-                for i in range(2, 3):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
-                    if 0 <= nx < 100 and 0 <= ny < 100 and ladders[nx][ny]:
-                        x = nx
-                        y = ny
-                        toggle = not toggle
-                        break
-                if not toggle:
-                    y += yd
+    x, y = end[0], end[1]
+    while x > 0:
+        for i in range(3):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < 100 and 0 <= ny < 100 and ladders[nx][ny] == 1:
+                ladders[x][y] = 0 # 지나간 사다리는 0으로 처리해서 다시 돌아가지 않도록!
+                x, y = nx, ny
 
-            if ladders[x][y] == 2:
-                print(f'#{tc} {sy}')
-                break
+    print(f'#{tc} {y}')
