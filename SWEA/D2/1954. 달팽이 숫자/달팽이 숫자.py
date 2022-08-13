a874bcc2e3121c6b5fa63dc621ffa1d2c84b6fbe
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-13 23:53:44 +0900

    [D2] Title: 달팽이 숫자, Time: 137 ms, Memory: 57,196 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,33 +1,28 @@
+# 델타 탐색을 위한 방향 인덱스
+dx = [0, 1, 0, -1] # 동 남 서 북
+dy = [1, 0, -1, 0]
+
 t = int(input())
 for tc in range(1, t + 1):
     n = int(input())
-    snail = [[0] * n for _ in range(n)]
+    snail = [[0] * n for _ in range(n)] # 달팽이 2차원 리스트
 
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-
-    x, y = 0, -1
-    idx = 0
-    num = 0
-
-    loop = n
-    while loop >= 0:
-        if loop == n:
-            for _ in range(loop):
-                x += dx[idx]
-                y += dy[idx]
-                num += 1
-                snail[x][y] = num
-            idx = (idx + 1) % 4
+    x, y, idx = 0, 0, 0 # 초기값 세팅
+    for i in range(1, n ** 2 + 1):
+        snail[x][y] = i
+        
+        # 달팽이가 이동합니다
+        nx = x + dx[idx]
+        ny = y + dy[idx]
+        
+        # 다음 인덱스가 범위 내이고, 아직 지나가지 않은 칸이라면 이동
+        if 0 <= nx < n and 0 <= ny < n and not snail[nx][ny]:
+            x, y = nx, ny
         else:
-            for _ in range(2):
-                for _ in range(loop):
-                    x += dx[idx]
-                    y += dy[idx]
-                    num += 1
-                    snail[x][y] = num
-                idx = (idx + 1) % 4
-        loop -= 1
+            # 방향 인덱스는 3을 넘지 않도록 4로 나눈 나머지로 업뎃!
+            idx = (idx + 1) % 4
+            x += dx[idx]
+            y += dy[idx]
 
     print(f'#{tc}')
     for i in range(n):
