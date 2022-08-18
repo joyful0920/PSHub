f49f9f7930f7223433748554fd9fb1d4aeca23d9
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-18 09:52:59 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,27 +1,31 @@
 import sys
-from collections import deque
+
+# sys.stdin = open('input.txt')
 si = sys.stdin.readline
+
+
+def dfs(v):                             # DFS by 재귀
+    global cnt
+    cnt += 1                            # 감염 +1
+    visited[v] = True                   # 방문한 컴은 방문 처리
+
+    for w in graph[v]:                  # 현재 컴과 연결된 다른 컴 확인
+        if not visited[w]:              # 방문 안한 컴이 있다면
+            v = w                       # 다음 방문할 컴 번호 업뎃
+            dfs(v)                      # 해당 컴으로 DFS 재귀 수행
+
 
 n = int(si())
 e = int(si())
-graph = [deque() for _ in range(n + 1)]
 
-for _ in range(e):
+graph = [[] for _ in range(n + 1)]      # 빈 인접 리스트 for DFS
+visited = [False] * (n + 1)             # visited 리스트 for DFS
+for _ in range(e):                      # # 간선정보 입력받아 grpah에 등록
     a, b = map(int, si().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def bfs(start):
-    queue = deque()
-    queue.append(start)
-    visited = deque()
-    visited.append(start)
-    while queue:
-        now = queue.popleft()
-        for i in graph[now]:
-            if i not in visited:
-                queue.append(i)
-                visited.append(i)
-    print(len(visited) - 1)
+cnt = -1                                # cnt 수는 -1로 초기화(1번 컴 제외)
+dfs(1)
 
-bfs(1)
+print(cnt)
