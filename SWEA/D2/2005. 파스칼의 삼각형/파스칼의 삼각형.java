7ab906b714aff1007e73949c6f5354c165590d11
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-18 17:15:27 +0900

    [D2] Title: 파스칼의 삼각형, Time: 165 ms, Memory: 22,580 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,37 +1,39 @@
+//import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("src/input.txt"));
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
 		
+		int t = sc.nextInt();
 		for (int tc = 1; tc <= t; tc++) {
 			int n = sc.nextInt();
-			int[][] arr = new int[n][n];
+			
+			ArrayList<Integer>[] dp = new ArrayList[n];
+			for (int i = 0; i < n; i++) {
+				dp[i] = new ArrayList<Integer>();
+				for (int j = 0; j <= i; j++) {
+					dp[i].add(1);
+				}
+			}
+			
+			for (int i = 2; i < n; i++) {
+				for (int j = 1; j < i; j++) {
+					dp[i].set(j, dp[i - 1].get(j - 1) + dp[i - 1].get(j));
+				}
+			}
 			
 			System.out.printf("#%d%n", tc);
 			for (int i = 0; i < n; i++) {
-				if (i == 0) { 
-					arr[0][0] = 1;
-					System.out.println("1");
+				for (int j = 0; j <= i; j++) {
+					System.out.printf("%d ", dp[i].get(j));
 				}
-				else {
-					for (int j = 0; j < i + 1; j++)  {
-						if (j == 0) {
-							arr[i][j] = 1;
-							System.out.print("1 ");
-						} else if (j == i) {
-							arr[i][j] = 1;
-							System.out.printf("1%n");
-						}
-						else {
-							arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
-							System.out.printf("%d ", arr[i][j]);
-						}
-					}
-				}
+				System.out.println();
 			}
+			
 		}
 	}
 
