eae632357fbf9d6700cd77d5d8954baf1a2d14fc
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-08-25 16:45:51 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 231 ms, Memory: 59,840 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -5,19 +5,18 @@ from collections import deque
 
 def bfs(x, y):
     q = deque([(x, y)])     # 큐에 일단 시작점 넣고
-    visited[x][y] = True    # 방문 처리까지
+    visited[x][y] = True    # 시작점 방문 처리
 
     while q:                # 큐에 원소가 있는 동안 반복
         x, y = q.popleft()  # 위치 하나 뽑고
         for i in range(4):  # 델타 탐색으로 인접한 주변 위치 확인
             nx = x + dx[i]
-            ny = y + dy[i]                  # 범위내 & 미방문 & 벽이 아니고
+            ny = y + dy[i]              # 범위내 & 미방문 & 벽이 아니고
             if 0 <= nx < 16 and 0 <= ny < 16 and not visited[nx][ny] and maze[nx][ny] != 1:
-                if maze[nx][ny] == 3:       # 도착점이면 1 리턴
+                if maze[nx][ny] == 3:   # 도착점이면 1 리턴
                     return 1
-                else:                       # 도착점이 아닌 통로라면
-                    q.append((nx ,ny))      # 해당 위치 큐에 삽입
-                    visited[nx][ny] = True  # 방문 처리  
+                q.append((nx, ny))      # 통로라면 큐에 삽입
+                visited[nx][ny] = True  # 방문 처리
 
     return 0    # 도달 불가능하면 0 리턴
 
