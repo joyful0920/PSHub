e17a5c2307c926239710666115056d953e724fbc
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-09-08 15:07:08 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+//import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+//		System.setIn(new FileInputStream("src/input.txt"));
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		
+		System.out.println(year - 543);
+	}
+
+}
