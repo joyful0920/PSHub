894e09421ed0bc0a98a44aa10579f6d12b7f7ce8
yongjun yi <81509328+joyful0920@users.noreply.github.com>
2022-09-12 15:22:59 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 211 ms, Memory: 62,464 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -1,15 +1,15 @@
 for tc in range(1, 11):
     n = int(input())
-    magnetics = [list(map(int, input().split())) for _ in range(n)]
+    table = [list(map(int, input().split())) for _ in range(n)]
 
-    result = 0
+    cnt = 0
     for j in range(n):
-        now = 0
-        for i in range(n):              # 열 우선 순회
-            if magnetics[i][j] == 2:    # n극이 등장
-                if now == 1:            # s극이 등장한 적이 있다면
-                    result += 1         # 교착 상태 + 1
-            if magnetics[i][j] != 0:    # 자석을 만나면
-                now = magnetics[i][j]   # now에 해당 극 저장
+        temp = 0                        # 마지막으로 확인한 자성체 종류를 자장시킬 변수
+        for i in range(n):
+            if table[i][j] != 0:        # 자성체가 존재하는 칸이고
+                if table[i][j] == 2:    # S극이라면
+                    if temp == 1:       # 마지막 확인한 자성체가 N극일 때만
+                        cnt += 1        # 교착 상태 수 + 1
+                temp = table[i][j]      # 마지막 확인한 자성체 종류 업뎃
 
-    print(f'#{tc} {result}')
+    print(f'#{tc} {cnt}')
