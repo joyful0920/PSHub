c5dee931ff220fd3718de4993eca321fedc32227
joyful0920 <geunseong0920@gmail.com>
2026-07-12 00:05:59 +0900

    Add practice solution: remove vowels from a string

---
+++ algorithms/strings/remove-vowels/main.ts
@@ -0,0 +1,22 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const str: string = lines[0];
+  return str.replace(/[aeiouAEIOU]/g, "");
+}
