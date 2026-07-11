da7d97e08d687df4fc328ad720eab85f8ffd3c2b
joyful0920 <geunseong0920@gmail.com>
2026-07-11 13:47:37 +0900

    Add practice solution: nth character of a string

---
+++ algorithms/strings/nth-character/main.ts
@@ -0,0 +1,27 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const input: string = lines[0];
+
+  const [str, nStr]: string[] = input.split(" ");
+  const n: number = Number(nStr);
+
+  const result: string = str[n - 1];
+  return result;
+}
