dca633b5a73456c1259067d07aa3c8023802ef7e
joyful0920 <geunseong0920@gmail.com>
2026-07-11 15:31:25 +0900

    Add practice solution: initials from two words

---
+++ algorithms/strings/initials/main.ts
@@ -0,0 +1,23 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const [s1, s2]: string[] = lines[0].split(" ");
+
+  return `${s1[0]}.${s2[0]}`;
+}
