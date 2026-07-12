b095ebdbd168cc9db49fec4b352925f5496910bd
joyful0920 <geunseong0920@gmail.com>
2026-07-12 16:51:19 +0900

    Add practice solution: odd or even

---
+++ algorithms/math/odd-even/main.ts
@@ -0,0 +1,23 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const n: number = Number(lines[0]);
+
+  return n % 2 === 0 ? "even" : "odd";
+}
