08aae3a1239ece72e2ce89f19ba525f7a78f0bd1
joyful0920 <geunseong0920@gmail.com>
2026-07-14 17:58:56 +0900

    Add practice solution: absolute value

---
+++ algorithms/math/absolute-value/main.ts
@@ -0,0 +1,23 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: number = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): number {
+  const n: number = Number(lines[0]);
+
+  return Math.abs(n);
+}
