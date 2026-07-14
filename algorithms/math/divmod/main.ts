2baddd8ed3f956e24c78d94a84a92916f96889cf
joyful0920 <geunseong0920@gmail.com>
2026-07-14 18:03:18 +0900

    Add practice solution: quotient and remainder

---
+++ algorithms/math/divmod/main.ts
@@ -0,0 +1,26 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const [a, b]: number[] = lines[0].split(" ").map(Number);
+
+  const quotient: number = Math.floor(a / b);
+  const remainder: number = a % b;
+
+  return `${quotient} ${remainder}`;
+}
