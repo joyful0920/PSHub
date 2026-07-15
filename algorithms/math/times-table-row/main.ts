2533719a74e7093cf5d9a073cf486aabd6964b32
joyful0920 <geunseong0920@gmail.com>
2026-07-15 15:32:55 +0900

    Add practice solution: multiples of n from 1 to 9

---
+++ algorithms/math/times-table-row/main.ts
@@ -0,0 +1,26 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const n: number = Number(lines[0]);
+
+  const answers: number[] = [];
+  for (let i: number = 1; i < 10; i++) answers.push(n * i);
+
+  return answers.join(" ");
+}
