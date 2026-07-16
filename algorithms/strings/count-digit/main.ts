0c9bf08d8bacb92582b25dd735c7c79fc20438da
joyful0920 <geunseong0920@gmail.com>
2026-07-16 18:17:26 +0900

    Add practice solution: count digit occurrences in a string

---
+++ algorithms/strings/count-digit/main.ts
@@ -0,0 +1,26 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: number = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): number {
+  const n: string = lines[0];
+
+  let cnt: number = 0;
+  for (const c of lines[1]) if (c === n) cnt += 1;
+
+  return cnt;
+}
