ed5cfa1b03457cf7870303801282441300ef445a
joyful0920 <geunseong0920@gmail.com>
2026-07-17 17:08:56 +0900

    Add practice solution: hours to days rounded up

---
+++ algorithms/math/hours-to-days/main.ts
@@ -0,0 +1,24 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: number = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): number {
+  const t: number = Number(lines[0]);
+
+  // 端数があれば 1 日繰り上げる = 切り上げ除算。
+  return Math.ceil(t / 24);
+}
