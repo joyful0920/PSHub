93c9aa566e054c28792f1547c08a5b4d1bcddbdd
joyful0920 <geunseong0920@gmail.com>
2026-07-17 14:27:25 +0900

    Add practice solution: 2x2 matrix determinant

---
+++ algorithms/math/determinant-2x2/main.ts
@@ -0,0 +1,27 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: number = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): number {
+  const grid: number[][] = lines.map((line: string) =>
+    line.split(" ").map(Number),
+  );
+
+  const [[a, b], [c, d]]: number[][] = grid;
+
+  return a * d - b * c;
+}
