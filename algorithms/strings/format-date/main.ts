1f96063a6bf9d53fd7aa5fd91562ebbc416b1e65
joyful0920 <geunseong0920@gmail.com>
2026-07-17 17:04:41 +0900

    Add practice solution: format year month day with slashes

---
+++ algorithms/strings/format-date/main.ts
@@ -0,0 +1,23 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: string = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): string {
+  const [y, m, d]: number[] = lines[0].split(" ").map(Number);
+
+  return `${y}/${m}/${d}`;
+}
