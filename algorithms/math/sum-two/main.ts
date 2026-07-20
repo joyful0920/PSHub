e9752ef0cc11cd03bb52215cfe69d0a704e5534e
joyful0920 <geunseong0920@gmail.com>
2026-07-20 22:33:00 +0900

    Add practice solution: sum of two integers

---
+++ algorithms/math/sum-two/main.ts
@@ -0,0 +1,24 @@
+import * as readline from "node:readline";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const lines: string[] = [];
+const reader: readline.Interface = readline.createInterface({
+  input: process.stdin,
+});
+
+reader.on("line", (line: string) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  const answer: number = solve(lines);
+  console.log(answer);
+});
+
+function solve(lines: string[]): number {
+  const a: number = Number(lines[0]);
+  const b: number = Number(lines[1]);
+
+  return a + b;
+}
